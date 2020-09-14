b3baa16d063f9065629b09e658dc3006154ca705
Vikram <vikram.bedi@paytm.com>
2020-09-14 10:34:40 +0530

    Displaying board on screen as array

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 
 function Board({ board }) {
-  console.log(board);
+  //console.log(board);
   //Array of array each array has length 8
   // first 2 are filed and last 2 are filed with pieces rest null
-  return <div></div>;
+  return (
+    <div className="board">
+      {board.map((piece, i) => {
+        return (
+          <div key={i}>
+            <p>{JSON.stringify(piece)}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Board;
