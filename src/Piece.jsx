ece0060adbe0efb0ea2c54d3b9041fb30ac1b906
Vikram <vikram.bedi@paytm.com>
2020-09-14 17:44:31 +0530

    Added moview pieces and drag preview

--- src/Piece.jsx
+++ src/Piece.jsx
@@ -1,11 +1,28 @@
 import React from "react";
+import { useDrag, DragPreviewImage } from "react-dnd";
 
 function Piece({ piece: { type, color } }) {
+  const [{ isDragging }, drag, preview] = useDrag({
+    item: { type: "piece", id: `${type}_${color}` },
+    collect: (monitor) => {
+      return {
+        isDragging: !!monitor.isDragging(),
+      };
+    },
+  });
   const pieceImage = require(`./assets/${type}_${color}.png`);
+
   return (
-    <div className="piece-container">
-      <img src={pieceImage} className="piece" />
-    </div>
+    <>
+      <DragPreviewImage connect={preview} src={pieceImage} />
+      <div
+        className="piece-container"
+        ref={drag}
+        style={{ opacity: isDragging ? 0 : 1 }}
+      >
+        <img src={pieceImage} className="piece" alt="chess piece" />
+      </div>
+    </>
   );
 }
 
